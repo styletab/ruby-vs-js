b5ba3463ed3f1da3caba602e883c5ef732b86e91
Jeffrey Horn <jeffh@generalassemb.ly>
2016-02-09 13:03:50 -0500

    Demo on Tuesday, February 9, 2016

---
+++ lib/ruby-vs-js.rb
@@ -0,0 +1,107 @@
+# puts 'Hello, Rubber Ducky!'
+
+# platypus = 'batman'
+# puts platypus
+
+# bruce_wayne = 'batman'
+# puts bruce_wayne
+
+# BruceWayne = 'batman'
+
+# 'bat\'man' # two special characters: \ '
+# identity = "Bruce Wayne"
+# puts "batman is really #{identity}"
+
+# def my_method
+#   1 + 1
+# end
+#
+# puts my_method
+#
+# puts 2
+
+# puts 3.even?
+# puts 4.even?
+
+# def even?
+# end
+
+# def divisible_by_3?(number)
+#   number % 3 == 0
+# end
+#
+# divisible_by_3?(4)
+
+# puts 3.is_a?(Number)
+# puts 3.is_a?(Fixnum)
+# puts 3.class
+# puts Fixnum.class
+
+# puts (3.333).class
+
+# [false, nil].each do |element|
+#   puts "#{element} is #{!!element}"
+# end
+
+# [false, nil, 0, -0, ''].each do |element|
+#   puts "#{element} is #{!!element}"
+# end
+
+# 1/"jeff" # error
+# 1/"2" # error
+# 1/"2".to_i #=> 0
+
+# duck = false
+
+# if duck
+#   puts 'Rubber Ducky'
+# else
+#   puts 'Sad.'
+# end
+
+# if duck
+#   puts 'Rubber Ducky'
+# end
+
+# dog = true
+#
+# if duck
+#   puts 'Rubber Ducky'
+# elsif dog
+#   puts 'Woof'
+# else
+#   puts 'Sad.'
+# end
+
+# numbers = [1, 2, 3]
+
+# numbers.map do |number|
+#   puts number
+# end
+#
+# numbers.map { |number| puts number }
+
+# (1..3).map { |number| puts number }
+# (1..3) != (1...3)
+# (1..3).each { |number| puts number }
+
+# 1.upto(3) do |number|
+#   puts number
+# end
+
+# cat = 'Fluffy'
+#
+# say = if cat
+#         'Meow'
+#       else
+#         "Shut up, I'm trying to sleep!"
+#       end
+#
+# puts say
+
+# def expensive_calculation
+#   puts "I'm iterating a metric ton of times"
+# end
+#
+# result ||= expensive_calculation
+# result ||= expensive_calculation
