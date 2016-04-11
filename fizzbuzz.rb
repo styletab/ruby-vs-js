7e122cd87208d076c39a72b2b2703929f62e7579
laurenfazah <laurenfazah@gmail.com>
2016-04-11 09:18:15 -0400

    added do to for block

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,7 @@ require 'pry'
 # Let `max_num` be the number that you want to count to.
 
 # ruby solution
-# for i in 1..max_num
+# for i in 1..max_num do
 #   if i % 3 == 0 && i % 5 == 0
 #    puts "fizzbuzz"
 #   elsif i % 3 == 0
@@ -24,7 +24,7 @@ require 'pry'
 #     other: []
 #   }
 #
-#   for i in 1..max_num
+#   for i in 1..max_num do
 #     if i % 3 == 0 && i % 5 == 0
 #       fb[:fizzbuzz] << i
 #     elsif i % 3 == 0
@@ -47,7 +47,7 @@ require 'pry'
 #     "other" => []
 #   }
 #
-#   for i in 1..max_num
+#   for i in 1..max_num do
 #     if i % 3 == 0 && i % 5 == 0
 #       fb["fizzbuzz"] << i
 #     elsif i % 3 == 0
