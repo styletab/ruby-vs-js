5ac7dbe13828aa0c29ecd372093154b7132f80c5
payne-chris-r <06cpayne@gmail.com>
2016-06-07 09:22:41 -0400

    Adds own solution and fixes #5
    Added my own solution because I wanted to figure it out in ruby_vs_js.rb.
    I also modified Lauren's due to issue #5 as suggested by @gaand

--- lib/ruby_vs_js.rb
+++ lib/ruby_vs_js.rb
@@ -1,35 +1,53 @@
-length = 25
+require 'pry'
 
-for i in 1.upto(length) do
-  if i % 3 == 0
-    if i % 5 == 0
-      puts 'Fizzbuzz'
-    else puts 'Fizz'
+# for i in 1.upto(length) do
+#   if i % 3 == 0
+#     if i % 5 == 0
+#       puts 'Fizzbuzz'
+#     else puts 'Fizz'
+#     end
+#   elsif i % 5 == 0
+#     puts 'Buzz'
+#   else
+#     puts i
+#   end
+# end
+
+# for i in 1.upto(length) do
+#   if i % 3 == 0
+#     if i % 5 == 0
+#       fizz_buzz['fizzbuzz'].push(i)
+#     else fizz_buzz['fizz'].push(i)
+#     end
+#   elsif i % 5 == 0
+#     fizz_buzz['buzz'].push(i)
+#   else
+#     fizz_buzz['other'].push(i)
+#   end
+# end
+
+def fizzbuzzer(num)
+  fizz_buzz = {
+    fizz: [],
+    buzz: [],
+    fizzbuzz: [],
+    other: []
+  }
+  for i in 1.upto(num) do
+    if i % 3 == 0
+      if i % 5 == 0
+        fizz_buzz[:fizzbuzz].push(i)
+      else fizz_buzz[:fizz].push(i)
+      end
+    elsif i % 5 == 0
+      fizz_buzz[:buzz].push(i)
+    else
+      fizz_buzz[:other].push(i)
     end
-  elsif i % 5 == 0
-    puts 'Buzz'
-  else
-    puts i
   end
+  puts 'fizz_buzz is '
+  puts fizz_buzz
 end
 
-
-fizz_buzz = {}
-
-fizz_buzz['fizz'] = []
-fizz_buzz['buzz'] = []
-fizz_buzz['fizzbuzz'] = []
-fizz_buzz['other'] = []
-
-for i in 1.upto(length) do
-  if i % 3 == 0
-    if i % 5 == 0
-      puts 'Fizzbuzz'
-    else puts 'Fizz'
-    end
-  elsif i % 5 == 0
-    puts 'Buzz'
-  else
-    puts i
-  end
-end
+binding.pry
+''
